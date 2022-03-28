74ae5a42022f0ba32fd451a331cd62a527ac1800
sgdvesus <jesusvlzez@gmail.com>
2022-03-28 14:41:09 -0600

    feat: add script with arguments

---
+++ 4_arguments.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# Script with arguments
+courseName=$1
+courseTime=$2
+
+echo "Course: $courseName. Time $courseTime."
+echo "Count total parameters: $#"
+echo "Send parameters: $*"
+echo "Script: $0"
