c1b0f09ead9985c4c7370c4b051366e6c2cce124
sgdvesus <jesusvlzez@gmail.com>
2022-03-28 12:14:31 -0600

    feat: add user and environment variables

---
+++ 2_var_b.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+# var declaretions
+echo "Result of import var name: $name ."
+
---
+++ 2_variables.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# var declaretions
+
+option=0
+name=Alexander
+
+echo "Option: $option and name: $name"
+
+# export var 
+export name
+
+# call script of destiny
+./2_var_b.sh
+
